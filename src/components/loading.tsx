f62e9103ebbc55298f0d708f9470980ca318bf26
rynxiao <yuzhongzi91@sina.com>
2020-04-08 10:48:19 +0800

    feat: add image loader

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import styles from './index.module.scss';
+
+const spinnerIcon = require('../assets/icn-spinner.svg');
 
 const Loading: React.FC = () => (
-  <div>loading</div>
+  <div className={styles.loading}>
+    <div className={styles.icon} dangerouslySetInnerHTML={{ __html: spinnerIcon }} />
+  </div>
 );
 
 export default Loading;
