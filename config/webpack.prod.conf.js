1d43dda11872c98b9e60e72077dff19193f5b774
rynxiao <yuzhongzi91@sina.com>
2020-04-09 11:08:48 +0800

    feat: config react externals

--- config/webpack.prod.conf.js
+++ config/webpack.prod.conf.js
@@ -2,8 +2,9 @@ const merge = require('webpack-merge');
 
 const path = require('path');
 
-const distPath = path.resolve(__dirname, '../dist');
-const srcPath = path.resolve(__dirname, '../src');
+const root = path.resolve(__dirname, '..');
+const distPath = path.resolve(root, 'dist');
+const srcPath = path.resolve(root, 'src');
 const base = require('./webpack.base.conf');
 
 module.exports = merge(base, {
@@ -11,14 +12,37 @@ module.exports = merge(base, {
   devtool: 'source-map',
   entry: path.join(srcPath, 'index.tsx'),
   output: {
-    library: 'react-image',
-    libraryTarget: 'commonjs2',
+    library: 'ReactImage',
+    libraryTarget: 'umd',
     path: distPath,
     filename: 'index.js',
   },
   externals: {
-    react: 'react',
-    'react-dom': 'react-dom',
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'ReactDOM',
+      root: 'ReactDOM',
+    },
+    'prop-types': {
+      commonjs: 'prop-types',
+      commonjs2: 'prop-types',
+      amd: 'PropTypes',
+      root: 'PropTypes',
+    },
+  },
+  resolve: {
+    alias: {
+      react: path.resolve(root, './node_modules/react'),
+      'react-dom': path.resolve(root, './node_modules/react-dom'),
+      'prop-types': path.resolve(root, './node_modules/prop-types'),
+    },
   },
   optimization: {
     minimize: true,
